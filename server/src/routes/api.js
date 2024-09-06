1be1d4ba8bcbc1443630ee275af19b7e1f2f0d49
Somil <somil.dhiman.02@gmail.com>
2024-09-07 03:27:30 +0530

    Added insert Todo API

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -4,6 +4,7 @@ import Login from "../contollers/login.controller.js";
 import { registerSchema } from "../validationschema/registerSchema.js";
 import { loginSchema } from "../validationschema/loginSchema.js";
 import { createTodo } from "../contollers/Todo.controller.js";
+import { check } from "express-validator";
 
 const apiRoute = express.Router();
 export const apiProtected = express.Router(); 
@@ -12,6 +13,8 @@ apiRoute.post('/register', registerSchema, Register);
 apiRoute.post('/login',loginSchema,Login);
 
 //Protected APIs
-apiProtected.post("/createTodo",createTodo);
+apiProtected.post("/createTodo",
+    [check("desc" ,"Todo desc is required ").exists()],
+    createTodo);
 
 export default apiRoute;
